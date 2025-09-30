23813646c018a3ab9e406fa728708227a8938da1
Ssweeney20 <ssweeney20@gmail.com>
2025-09-30 16:25:28 -0400

    changed to browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext';
 
 import WarehouseBrowser from './pages/WarehouseBrowser';
